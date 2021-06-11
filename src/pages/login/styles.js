0204ff143812a9be76d829c506a88a941342cc6f
anachfigueira <anachfigueira@gmail.com>
2021-06-11 12:21:00 -0300

    Making FormContainer responsive

--- src/pages/login/styles.js
+++ src/pages/login/styles.js
@@ -7,11 +7,17 @@ export const Container = styled.div`
 `;
 
 export const FormContainer = styled.div`
-    width: 30vw;
+    width: 50vw;
     margin: 0 auto;
     box-shadow: 2px 2px 16px -1px rgba(0,0,0,0.25);
     border-radius: 4px;
     padding: 32px;
+
+    @media (max-width: 1200px) {
+        & {
+            width: 70% !important;
+        }
+    }
 `;
 
 
